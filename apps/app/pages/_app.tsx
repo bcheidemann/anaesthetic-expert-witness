e360421f32e60680fad1859e4409833694c6b9be
Ben Heidemann <ben@heidemann.co.uk>
2022-11-26 17:33:47 +0000

    configure app boilerplate

--- apps/app/pages/_app.tsx
+++ apps/app/pages/_app.tsx
@@ -1,18 +1,36 @@
-import { AppProps } from 'next/app';
+import { AppProps as NextAppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { muiTheme } from '../config/theme';
+import createEmotionCache from '../config/createEmotionCache';
 
-function CustomApp({ Component, pageProps }: AppProps) {
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+const clientSideEmotionCache = createEmotionCache();
+
+interface AppProps extends NextAppProps {
+  emotionCache?: EmotionCache;
+}
+
+function App(props: AppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
   return (
-    <>
+    <CacheProvider value={emotionCache}>
       <Head>
-        <title>Welcome to app!</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <main className="app">
+      <ThemeProvider theme={muiTheme}>
+        <CssBaseline />
         <Component {...pageProps} />
-      </main>
-    </>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
-export default CustomApp;
+export default App;
